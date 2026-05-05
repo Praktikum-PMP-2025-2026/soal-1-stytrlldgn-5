0a83191dc6f274c80a58dc5fb7537bc53bb87b64
stytrlldgn <satyaadnyanaq@gmail.com>
2026-05-05 09:05:53 +0700

    Update soal1.c

--- soal1.c
+++ soal1.c
@@ -1,5 +1,73 @@
+/** EL2008 Praktikum Pemecahan Masalah dengan Pemrograman 2024/2025
+*   Modul               : 4 - Dynamic Structures
+*   Hari dan Tanggal    : selasa, 5 Mei 2026
+*   Nama (NIM)          : I Ketut Satya Adnyana (13224076)
+*   Nama File           : soal1.c
+*   Deskripsi           : 
+*/
+//code biasa dengan data manipulasi
 #include <stdio.h>
+#include <string.h>
+#include <stdbool.h>
+#define MAX 1000
+int main() {
+    char str[MAX];
+    bool hapus[MAX] = {false}; 
+    int stack[MAX];
+    int top = -1;
+    if (fgets(str, MAX, stdin) == NULL) return 0;
+    str[strcspn(str, "\n")] = 0;
 
-int main(void) {
+    int n = strlen(str);
+
+    for (int i = 0; i < n; i++) {
+        if (str[i] == '(') {
+            stack[++top] = i;
+        } 
+        else if (str[i] == ')') {
+            if (top >= 0) {
+                top--;
+            } else {
+                printf("INVALID");
+            }
+        }
+    }
+
+//untuk {}
+    for (int i = 0; i < n; i++) {
+        if (str[i] == '{') {
+            stack[++top] = i;
+        } 
+        else if (str[i] == '}') {
+            if (top >= 0) {
+                top--;
+            } else {
+                printf("INVALID");
+            }
+        }
+    }
+
+    //untuk []
+    for (int i = 0; i < n; i++) {
+        if (str[i] == '[') {
+            stack[++top] = i;
+        } 
+        else if (str[i] == ']') {
+            if (top >= 0) {
+                top--;
+            } else {
+                printf("INVALID");
+            }
+        }
+    }
+
+    if (top >= 0){
+        hapus[stack[top--]] = true;
+        printf("INVALID");
+    }
+    else{
+        printf("VALID");
+    }
+
     return 0;
 }
