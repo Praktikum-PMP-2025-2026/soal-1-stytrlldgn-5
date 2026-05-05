197c47c8f0536c489ec6a5ed9a666e29a5532e5e
stytrlldgn <satyaadnyanaq@gmail.com>
2026-05-05 09:12:06 +0700

    Update soal1.c

--- soal1.c
+++ soal1.c
@@ -65,8 +65,11 @@ int main() {
         hapus[stack[top--]] = true;
         printf("INVALID");
     }
-    else{
+    else if(top >= 0){
         printf("VALID");
+    }
+    else {
+        printf(" ");
     }
 
     return 0;
